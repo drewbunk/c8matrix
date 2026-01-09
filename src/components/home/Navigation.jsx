0733e6bf20d2f6c83a396a228b42deca41fc421c
Drew Bunkley <lamont770@gmail.com>
2026-01-09 23:44:28 +0000

    File changes

--- src/components/home/Navigation.jsx
+++ src/components/home/Navigation.jsx
@@ -12,6 +12,7 @@ const navLinks = [
   { label: 'Projects', href: '#projects', type: 'anchor' },
   { label: 'Portfolio', href: '#portfolio', type: 'anchor' },
   { label: 'Shop', href: '#shop', type: 'anchor' },
+  { label: 'Investors', href: 'InvestorContact', type: 'page' },
   { label: 'Contact', href: '#contact', type: 'anchor' },
 ];
 
